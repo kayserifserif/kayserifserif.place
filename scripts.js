12913d9d5c2f89dd30939fb0a70e99bc51d23022
bookwormgirl910 <whykatherine@gmail.com>
2015-12-26 16:14:19 +0800

    troubleshooting

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	$("nav a").click(function() {
+		$("nav .current").removeClass("current");
+		$("#main section.current").removeClass("current");
+
+		$(this).addClass("current");
+		$("#main section" + $(this).attr("href")).addClass("current");
+		// $("#main section" + $(this).attr("href")).slideDown();
+	});
+});
