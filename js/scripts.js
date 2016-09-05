c08017908c5b068f7fa0f28e7aa73711ca49bdb9
bookwormgirl910 <whykatherine@gmail.com>
2016-09-05 15:40:02 +0800

    add videos

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@
 $(document).ready(function() {
 	$("nav a").click(function() {
 		$("nav .current").removeClass("current");
-		$("#main section.current").removeClass("current");
+		$(".main section.current").removeClass("current");
 
 		$(this).addClass("current");
-		$("#main section" + $(this).attr("href")).addClass("current");
+		$(".main section" + $(this).attr("href")).addClass("current");
 		// $("#main section" + $(this).attr("href")).slideDown();
 	});
 });
