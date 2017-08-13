df8839e12eb12137542b1a92ec29a85c886f698c
bookwormgirl910 <whykatherine@gmail.com>
2017-08-13 15:13:12 +0800

    move old versions

---
+++ v1/js/scripts.js
@@ -0,0 +1,12 @@
+// DEBUG
+
+$(document).ready(function() {
+	$("nav a").click(function() {
+		$("nav .current").removeClass("current");
+		$(".main section.current").removeClass("current");
+
+		$(this).addClass("current");
+		$(".main section" + $(this).attr("href")).addClass("current");
+		// $("#main section" + $(this).attr("href")).slideDown();
+	});
+});
