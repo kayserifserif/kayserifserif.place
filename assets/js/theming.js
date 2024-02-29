902e5617b50cce0bcae593f0ba5e862280318232
Katherine Yang <whykatherine@gmail.com>
2024-02-28 22:16:05 -0500

    move things around

--- assets/js/theming.js
+++ assets/js/theming.js
@@ -1,18 +1,23 @@
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-prefersDarkScheme.addEventListener("change", e => {
+prefersDarkScheme.addEventListener("change", handleMediaQueryChange);
+
+function handleMediaQueryChange(e) {
   const { matches } = e;
   const theme = matches ? "dark" : "light";
   setTheme(theme);
-});
+}
 
 const themeButtons = document.querySelectorAll(".theme-button");
 themeButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    const value = button.value;
-    setTheme(value);
-    localStorage.setItem("theme", value);
-  });
+  button.addEventListener("click", handleThemeButtonClick);
 });
+
+function handleThemeButtonClick(event) {
+  const button = event.target;
+  const value = button.value;
+  setTheme(value);
+  localStorage.setItem("theme", value);
+}
 
 function setTheme(value) {
   if (value === "system") {
